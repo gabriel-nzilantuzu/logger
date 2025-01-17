34305612db83eb57bd2ed250b23cc1a4bd3d7ea5
gabriel nzilantuzu <gabrielnzilantuzu@gmail.com>
2025-01-17 15:51:57 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,47 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importing Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  // State to store WebSocket messages
   const [messages, setMessages] = useState<string[]>([]);
   const [isConnected, setIsConnected] = useState<boolean>(false);
 
   useEffect(() => {
-    // Function to initialize WebSocket and handle connection, message reception, etc.
-    const createSocket = () => {
-      const socket = new WebSocket('ws://log-analytics.ns.namespaxe.com/logger');
 
-      // WebSocket connection open
+    const createSocket = () => {
+      const socket = new WebSocket('wss://log-analytics.ns.namespaxe.com/logger');
+
       socket.onopen = () => {
         setIsConnected(true);
         console.log('WebSocket Connected');
       };
 
-      // WebSocket onmessage event
       socket.onmessage = (event) => {
         console.log('Received message:', event.data);
-        setMessages((prevMessages) => [...prevMessages, event.data]); // Append new message
+        setMessages((prevMessages) => [...prevMessages, event.data]);
       };
 
-      // WebSocket onclose event
       socket.onclose = () => {
         setIsConnected(false);
         console.log('WebSocket Disconnected');
-        // Reconnect after 2 seconds if the connection is closed
+
         setTimeout(createSocket, 2000);
       };
 
-      // WebSocket onerror event
       socket.onerror = (error) => {
         console.error('WebSocket error:', error);
-        socket.close(); // Close the connection on error to trigger reconnection
+        socket.close();
       };
 
       return socket;
     };
 
-    // Create initial WebSocket connection
     const socket = createSocket();
 
-    // Cleanup WebSocket connection when the component unmounts
     return () => {
       socket.close();
     };
-  }, []); // Empty dependency array to run this effect only once
+  }, []);
 
   return (
     <div className="App container mt-4">
